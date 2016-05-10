6f445d3ce931dbfbba802bdbbb7987fae59f31a3
MrChuckchucky <b.devienne@rubika-edu.com>
2016-05-10 16:17:52 +0200

    Add bullet and shoot

--- Banane mécanique/Assets/Scripts/InputManager.cs
+++ Banane mécanique/Assets/Scripts/InputManager.cs
@@ -13,11 +13,18 @@ public class InputManager : MonoBehaviour
     public float backAngles;
     public float frontAngles;
     public float rotateSpeed;
+    public float coolDown;
+
+    float cdArmLeftP1;
+    float cdArmRightP1;
+    float cdArmLeftP2;
+    float cdArmRightP2;
 
     GameObject P1LeftArm;
     GameObject P1RightArm;
     GameObject P2LeftArm;
     GameObject P2RightArm;
+    GameObject bullet;
     // Use this for initialization
     void Start ()
     {
@@ -25,11 +32,18 @@ public class InputManager : MonoBehaviour
         P1RightArm = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).GetChild(0).gameObject;
         P2LeftArm = GameObject.FindGameObjectWithTag("Player").transform.GetChild(1).GetChild(1).gameObject;
         P2RightArm = GameObject.FindGameObjectWithTag("Player").transform.GetChild(1).GetChild(0).gameObject;
+
+        cdArmLeftP1 = cdArmRightP1 = cdArmLeftP2 = cdArmRightP2 = 0;
+        bullet = Resources.Load("Bullet") as GameObject;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        cdArmLeftP1 -= Time.deltaTime;
+        cdArmRightP1 -= Time.deltaTime;
+        cdArmLeftP2 -= Time.deltaTime;
+        cdArmRightP2 -= Time.deltaTime;
         if (!playerIndexSet || !prevState.IsConnected)
         {
             for (int i = 0; i < 4; ++i)
@@ -48,12 +62,13 @@ public class InputManager : MonoBehaviour
         prevState = state;
         state = GamePad.GetState(playerIndex);
 
-        MoveArmLeft(P1LeftArm, 1);
-        MoveArmRight(P1RightArm, 1);
-        MoveArmLeft(P2LeftArm, 2);
-        MoveArmRight(P2RightArm, 2);
+        Rotate();
+        MoveArms();
+    }
 
-        if((XInput.instance.getButton(1, 'L') == ButtonState.Released || XInput.instance.getButton(1, 'R') == ButtonState.Released) && (XInput.instance.getButton(2, 'L') == ButtonState.Released || XInput.instance.getButton(2, 'R') == ButtonState.Released))
+    void Rotate()
+    {
+        if ((XInput.instance.getButton(1, 'L') == ButtonState.Released || XInput.instance.getButton(1, 'R') == ButtonState.Released) && (XInput.instance.getButton(2, 'L') == ButtonState.Released || XInput.instance.getButton(2, 'R') == ButtonState.Released))
         {
             if (XInput.instance.getButton(1, 'L') == ButtonState.Pressed && XInput.instance.getButton(2, 'L') == ButtonState.Pressed)
             {
@@ -65,7 +80,13 @@ public class InputManager : MonoBehaviour
             }
         }
     }
-
+    void MoveArms()
+    {
+        MoveArmLeft(P1LeftArm, 1);
+        MoveArmRight(P1RightArm, 1);
+        MoveArmLeft(P2LeftArm, 2);
+        MoveArmRight(P2RightArm, 2);
+    }
     void MoveArmRight(GameObject arm, int index)
     {
         if(XInput.instance.getYStickRight(index) <= tolerance && XInput.instance.getYStickRight(index) >= -tolerance && XInput.instance.getXStickRight(index) <= tolerance && XInput.instance.getXStickRight(index) >= -tolerance)
@@ -88,6 +109,7 @@ public class InputManager : MonoBehaviour
                     arm.transform.localEulerAngles = new Vector3(0, backAngles, 0);
                 }
             }
+            CanShoot("Right", index);
         }
     }
     void MoveArmLeft(GameObject arm, int index)
@@ -112,6 +134,54 @@ public class InputManager : MonoBehaviour
                     arm.transform.localEulerAngles = new Vector3(0, 360 - backAngles, 0);
                 }
             }
+            CanShoot("Left", index);
         }
+    }
+    void CanShoot(string side, int index)
+    {
+        if(side == "Right")
+        {
+            if (index == 1)
+            {
+                if(cdArmRightP1 <= 0)
+                {
+                    Shoot(P1RightArm, index);
+                    cdArmRightP1 = coolDown;
+                }
+            }
+            else if (index == 2)
+            {
+                if (cdArmRightP2 <= 0)
+                {
+                    Shoot(P2RightArm, index);
+                    cdArmRightP2 = coolDown;
+                }
+            }
+        }
+        else if(side == "Left")
+        {
+            if (index == 1)
+            {
+                if (cdArmLeftP1 <= 0)
+                {
+                    Shoot(P1LeftArm, index);
+                    cdArmLeftP1 = coolDown;
+                }
+            }
+            else if (index == 2)
+            {
+                if (cdArmLeftP2 <= 0)
+                {
+                    Shoot(P2LeftArm, index);
+                    cdArmLeftP2 = coolDown;
+                }
+            }
+        }
+    }
+    void Shoot(GameObject arm, int index)
+    {
+        GameObject shoot = Instantiate(bullet, arm.transform.position, Quaternion.identity) as GameObject;
+        shoot.transform.LookAt(arm.transform.GetChild(0).position);
+        shoot.GetComponent<BulletScript>().playerIndex = index;
     }
 }
