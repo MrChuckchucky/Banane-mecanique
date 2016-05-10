c9e068da7598b6fbfd179670ab2c760cbca7f0fc
unknown <r.mathieu@J001-0140-15.etudiant.rubika-edu.com.local>
2016-05-10 16:29:11 +0200

    Modifs pour le spawn.

--- Banane mécanique/Assets/Editor/Wave/WaveEditor.cs
+++ Banane mécanique/Assets/Editor/Wave/WaveEditor.cs
@@ -1,162 +1,13 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
-using SimpleJSON;
-using System;
-using System.IO;
 
 public class WaveEditor : EditorWindow
 {
     protected static WaveEditor window;
 
-    public List<Wave> waves;
-    public List<float> timers;
+    public WaveController wave;
 
-    public bool isToggledTimers = false;
-    public bool isToggledWave = false;
-
-    public string fileName = "";
-
-    protected void AddWave()
-    {
-        Wave wave = new Wave();
-        wave.units = new List<Unit>();
-        waves.Add(wave);
-    }
-
-    protected void RemoveWave(int index)
-    {
-        if (waves.Count > index)
-        {
-            waves.RemoveAt(index);
-        }
-    }
-
-    protected void AddTimer(float t = 0.0f)
-    {
-        timers.Add(t);
-    }
-
-    protected void RemoveTimer(int index)
-    {
-        if (timers.Count > index)
-        {
-            timers.RemoveAt(index);
-        }
-    }
-
-    protected void Load()
-    {
-        waves = new List<Wave>();
-        timers = new List<float>();
-
-        JSONClass json = JSONNode.LoadFromFile(Application.persistentDataPath + "/WaveUnits/" + fileName + ".wave").AsObject;
-
-        for (int i = 0; i < json["timers"].Count; i++)
-        {
-            timers.Add(json["timers"][i].AsFloat);
-        }
-
-        for (int i = 0; i < json["waves"].Count; i++)
-        {
-            Wave wave = new Wave();
-            wave.units = new List<Unit>();
-            JSONClass jsonNodeOne = json["waves"][i].AsObject;
-            for (int j = 0; j < jsonNodeOne["unit"].Count; j++)
-            {
-                Unit unit = new Unit();
-                JSONClass nodeChild = jsonNodeOne["unit"][j].AsObject;
-                if(nodeChild["entity"] != null)
-                {
-                    unit.entity = Resources.Load("Prefabs/" + nodeChild["entity"]) as GameObject;
-                }
-                unit.spawnPoint.x = nodeChild["x"].AsFloat;
-                unit.spawnPoint.y = nodeChild["y"].AsFloat;
-                unit.spawnPoint.z = nodeChild["z"].AsFloat;
-                wave.units.Add(unit);
-            }
-            wave.timeDelay = jsonNodeOne["time"].AsFloat;
-
-            waves.Add(wave);
-        }
-
-        isToggledTimers = json["toggleTimer"].AsBool;
-        isToggledWave = json["toggleWave"].AsBool;
-        /*JSONNode j = JSONNode.LoadFromFile(Application.persistentDataPath + "/WaveUnits/" + fileName + ".wave");
-
-        window = JsonUtility.FromJson(j.ToString(), typeof(WaveEditor)) as WaveEditor;*/
-    }
-
-    protected void Save()
-    {
-        JSONClass json = new JSONClass();
-
-        for (int i = 0; i < timers.Count; i++)
-        {
-            json["timers"][i].AsFloat = timers[i];
-        }
-
-        for (int i = 0; i < waves.Count; i++)
-        {
-            Wave wave = waves[i];
-            JSONClass jsonNodeOne = json["waves"][i].AsObject;
-            for (int j = 0; j < wave.units.Count; j++)
-            {
-                Unit unit = wave.units[j];
-                JSONClass nodeChild = jsonNodeOne["unit"][j].AsObject;
-                if(unit.entity)
-                {
-                    nodeChild["entity"] = unit.entity.name;
-                }
-                else
-                {
-                    nodeChild["entity"] = "null";
-                }
-                nodeChild["x"].AsFloat = unit.spawnPoint.x;
-                nodeChild["y"].AsFloat = unit.spawnPoint.y;
-                nodeChild["z"].AsFloat = unit.spawnPoint.z;
-            }
-            jsonNodeOne["time"].AsFloat = wave.timeDelay;
-        }
-
-        json["toggleTimer"].AsBool = isToggledTimers;
-        json["toggleWave"].AsBool = isToggledWave;
-
-        Debug.Log(json.ToString());
-
-        json.SaveToFile(Application.persistentDataPath + "/WaveUnits/" + fileName + ".wave");
-
-        /*JSONData j = new JSONData(JsonUtility.ToJson(window));
-
-        j.SaveToFile(Application.persistentDataPath + "/WaveUnits/" + fileName + ".wave");*/
-    }
-}
-
-[Serializable]
-public class Unit
-{
-    public GameObject entity;
-    public Vector3 spawnPoint;
-}
-
-[Serializable]
-public class Wave
-{
-    public List<Unit> units = new List<Unit>();
-    public float timeDelay = 0.0f;
-
-    public void AddUnit()
-    {
-        units.Add(new Unit());
-    }
-
-    public void RemoveUnit(int index)
-    {
-        if(units.Count > index)
-        {
-            units.RemoveAt(index);
-        }
-    }
 }
 
 public class WaveEditorCreate : WaveEditor
@@ -165,60 +16,60 @@ public class WaveEditorCreate : WaveEditor
     static void CreateInit()
     {
         window = (WaveEditorCreate)EditorWindow.GetWindow(typeof(WaveEditorCreate));
-        window.waves = new List<Wave>();
-        window.timers = new List<float>();
+        window.wave = new WaveController();
         window.Show();
     }
 
     public static void CreateInit(WaveEditor editor)
     {
         CreateInit();
-        window.fileName = editor.fileName;
-        window.waves = editor.waves;
-        window.timers = editor.timers;
-        window.isToggledTimers = editor.isToggledTimers;
-        window.isToggledWave = editor.isToggledWave;
+        window.wave = new WaveController();
+        window.wave.fileName = editor.wave.fileName;
+        window.wave.waves = editor.wave.waves;
+        window.wave.timers = editor.wave.timers;
+        window.wave.isToggledTimers = editor.wave.isToggledTimers;
+        window.wave.isToggledWave = editor.wave.isToggledWave;
     }
 
     void OnGUI()
     {
-        fileName = EditorGUILayout.TextField("File Name", fileName);
+        wave.fileName = EditorGUILayout.TextField("File Name", wave.fileName);
 
-        if (GUILayout.Button("Save") && fileName != "")
+        if (GUILayout.Button("Save") && wave.fileName != "")
         {
-            Save();
+            wave.Save();
         }
 
-        isToggledTimers = EditorGUILayout.BeginToggleGroup("Edit Timers", isToggledTimers);
-        EditorGUILayout.BeginFadeGroup(isToggledTimers ? 1 : 0);
+        wave.isToggledTimers = EditorGUILayout.BeginToggleGroup("Edit Timers", wave.isToggledTimers);
+        EditorGUILayout.BeginFadeGroup(wave.isToggledTimers ? 1 : 0);
 
-        if(isToggledTimers)
+        if(wave.isToggledTimers)
         {
             int waveIterator = 0;
 
-            for (int i = 0; i < timers.Count; i++)
+            for (int i = 0; i < wave.timers.Count; i++)
             {
                 EditorGUILayout.BeginHorizontal();
-                if (timers[i] < -float.Epsilon)
+                if (wave.timers[i] < -float.Epsilon)
                 {
-                    if (waves.Count > waveIterator && waves[waveIterator] != null)
+                    if (wave.waves.Count > waveIterator && wave.waves[waveIterator] != null)
                     {
                         EditorGUILayout.LabelField("Wave n°" + (waveIterator + 1));
                     }
                     if (GUILayout.Button("-"))
                     {
-                        RemoveWave(waveIterator);
-                        RemoveTimer(i);
+                        wave.RemoveWave(waveIterator);
+                        wave.RemoveTimer(i);
                     }
                     waveIterator++;
                 }
                 else
                 {
-                    timers[i] = EditorGUILayout.FloatField(timers[i]);
+                    wave.timers[i] = EditorGUILayout.FloatField(wave.timers[i]);
 
                     if (GUILayout.Button("-"))
                     {
-                        RemoveTimer(i);
+                        wave.RemoveTimer(i);
                     }
                 }
                 EditorGUILayout.EndHorizontal();
@@ -231,24 +82,24 @@ public class WaveEditorCreate : WaveEditor
 
         if (GUILayout.Button("Add Timer"))
         {
-            AddTimer();
+            wave.AddTimer();
         }
 
         if (GUILayout.Button("Add Wave"))
         {
-            AddTimer(-5.0f);
-            AddWave();
+            wave.AddTimer(-5.0f);
+            wave.AddWave();
         }
 
-        isToggledWave = EditorGUILayout.BeginToggleGroup("Edit Waves", isToggledWave);
-        EditorGUILayout.BeginFadeGroup(isToggledWave ? 1 : 0);
+        wave.isToggledWave = EditorGUILayout.BeginToggleGroup("Edit Waves", wave.isToggledWave);
+        EditorGUILayout.BeginFadeGroup(wave.isToggledWave ? 1 : 0);
 
-        if(isToggledWave)
+        if(wave.isToggledWave)
         {
-            for (int i = 0; i < waves.Count; i++)
+            for (int i = 0; i < wave.waves.Count; i++)
             {
                 EditorGUILayout.LabelField("Wave n°" + (i + 1));
-                Wave w = waves[i];
+                Wave w = wave.waves[i];
                 EditorGUILayout.BeginVertical();
 
                 EditorGUILayout.BeginHorizontal();
@@ -296,16 +147,17 @@ public class WaveEditorLoad : WaveEditor
     static void LoadInit()
     {
         window = (WaveEditorLoad)EditorWindow.GetWindow(typeof(WaveEditorLoad));
+        window.wave = new WaveController();
         window.Show();
     }
 
     void OnGUI()
     {
-        fileName = EditorGUILayout.TextField("File Name", fileName);
+        wave.fileName = EditorGUILayout.TextField("File Name", wave.fileName);
 
-        if (GUILayout.Button("Load") && fileName != "")
+        if (GUILayout.Button("Load") && wave.fileName != "")
         {
-            Load();
+             wave = wave.Load();
             WaveEditorCreate.CreateInit(this);
         }
     }
--- Banane mécanique/Assets/Scripts/Manager/WaveManager.cs
+++ Banane mécanique/Assets/Scripts/Manager/WaveManager.cs
@@ -1,15 +1,232 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+using System.IO;
+
+public class WaveController
+{
+    public List<Wave> waves;
+    public List<float> timers;
+
+    public bool isToggledTimers = false;
+    public bool isToggledWave = false;
+
+    public string fileName = "";
+
+    public WaveController()
+    {
+        waves = new List<Wave>();
+        timers = new List<float>();
+    }
+
+    public void AddWave()
+    {
+        Wave wave = new Wave();
+        wave.units = new List<Unit>();
+        waves.Add(wave);
+    }
+
+    public void RemoveWave(int index)
+    {
+        if (waves.Count > index)
+        {
+            waves.RemoveAt(index);
+        }
+    }
+
+    public void AddTimer(float t = 0.0f)
+    {
+        timers.Add(t);
+    }
+
+    public void RemoveTimer(int index)
+    {
+        if (timers.Count > index)
+        {
+            timers.RemoveAt(index);
+        }
+    }
+
+    public WaveController Load()
+    {
+        WaveController wave;
+        Load(fileName, out wave);
+        return wave;
+    }
+
+    public static void Load(string fileName, out WaveController waveEdit)
+    {
+        waveEdit = null;
+        if (File.Exists(Application.persistentDataPath + "/WaveUnits/" + fileName + ".wave"))
+        {
+            JSONClass json = JSONNode.LoadFromFile(Application.persistentDataPath + "/WaveUnits/" + fileName + ".wave").AsObject;
+
+            waveEdit = new WaveController();
+
+            waveEdit.fileName = fileName;
+
+            waveEdit.waves = new List<Wave>();
+            waveEdit.timers = new List<float>();
+
+
+            for (int i = 0; i < json["timers"].Count; i++)
+            {
+                waveEdit.timers.Add(json["timers"][i].AsFloat);
+            }
+
+            for (int i = 0; i < json["waves"].Count; i++)
+            {
+                Wave wave = new Wave();
+                wave.units = new List<Unit>();
+                JSONClass jsonNodeOne = json["waves"][i].AsObject;
+                for (int j = 0; j < jsonNodeOne["unit"].Count; j++)
+                {
+                    Unit unit = new Unit();
+                    JSONClass nodeChild = jsonNodeOne["unit"][j].AsObject;
+                    if (nodeChild["entity"] != null)
+                    {
+                        unit.entity = Resources.Load("Prefabs/" + nodeChild["entity"]) as GameObject;
+                    }
+                    unit.spawnPoint.x = nodeChild["x"].AsFloat;
+                    unit.spawnPoint.y = nodeChild["y"].AsFloat;
+                    unit.spawnPoint.z = nodeChild["z"].AsFloat;
+                    wave.units.Add(unit);
+                }
+                wave.timeDelay = jsonNodeOne["time"].AsFloat;
+
+                waveEdit.waves.Add(wave);
+            }
+
+            waveEdit.isToggledTimers = json["toggleTimer"].AsBool;
+            waveEdit.isToggledWave = json["toggleWave"].AsBool;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public void Save()
+    {
+        JSONClass json = new JSONClass();
+
+        for (int i = 0; i < timers.Count; i++)
+        {
+            json["timers"][i].AsFloat = timers[i];
+        }
+
+        for (int i = 0; i < waves.Count; i++)
+        {
+            Wave wave = waves[i];
+            JSONClass jsonNodeOne = json["waves"][i].AsObject;
+            for (int j = 0; j < wave.units.Count; j++)
+            {
+                Unit unit = wave.units[j];
+                JSONClass nodeChild = jsonNodeOne["unit"][j].AsObject;
+                if (unit.entity)
+                {
+                    nodeChild["entity"] = unit.entity.name;
+                }
+                else
+                {
+                    nodeChild["entity"] = "null";
+                }
+                nodeChild["x"].AsFloat = unit.spawnPoint.x;
+                nodeChild["y"].AsFloat = unit.spawnPoint.y;
+                nodeChild["z"].AsFloat = unit.spawnPoint.z;
+            }
+            jsonNodeOne["time"].AsFloat = wave.timeDelay;
+        }
+
+        json["toggleTimer"].AsBool = isToggledTimers;
+        json["toggleWave"].AsBool = isToggledWave;
+
+        Debug.Log(json.ToString());
+
+        json.SaveToFile(Application.persistentDataPath + "/WaveUnits/" + fileName + ".wave");
+    }
+}
+
+public class Unit
+{
+    public GameObject entity;
+    public Vector3 spawnPoint;
+}
+
+public class Wave
+{
+    public List<Unit> units = new List<Unit>();
+    public float timeDelay = 0.0f;
+
+    public void AddUnit()
+    {
+        units.Add(new Unit());
+    }
+
+    public void RemoveUnit(int index)
+    {
+        if (units.Count > index)
+        {
+            units.RemoveAt(index);
+        }
+    }
+}
 
 public class WaveManager : MonoBehaviour {
+    
 
-	// Use this for initialization
-	void Start () {
-	
+    public List<string> waveNames;
+    List<WaveController> waves;
+    public bool isSpawning = true;
+    public float timeDelayBetweenMajorWaves = 2.0f;
+
+
+    void Start () {
+        waves = new List<WaveController>();
+        WaveController wave;
+        for (int i = 0; i < waveNames.Count; i++)
+        {
+            WaveController.Load(waveNames[i], out wave);
+            if (wave != null)
+            {
+                waves.Add(wave);
+            }
+        }
+        StartCoroutine(Spawn());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    IEnumerator Spawn()
+    {
+        while(isSpawning)
+        {
+
+            for (int i = 0; i < waves.Count; i++)
+            {
+                WaveController wave = waves[i];
+
+                for (int j = 0; j < wave.timers.Count; j++)
+                {
+                    if(wave.timers[j] < 0.0f)
+                    {
+                        for (int k = 0; k < wave.waves.Count; k++)
+                        {
+                            Wave wav = wave.waves[k];
+                            for (int l = 0; l < wav.units.Count; l++)
+                            {
+                                Instantiate(wav.units[l].entity, wav.units[l].spawnPoint, wav.units[l].entity.transform.rotation);
+                                yield return new WaitForSeconds(wav.timeDelay);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        yield return new WaitForSeconds(wave.timers[j]);
+                    }
+                }
+            }
+
+            yield return new WaitForSeconds(timeDelayBetweenMajorWaves);
+        }
+    }
 }
